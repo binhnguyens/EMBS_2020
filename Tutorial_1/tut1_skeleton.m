55312e2dc717a62e2e5292520515c206176f0bbb
Binh Nguyen <binhnguyens@live.ca>
2020-09-24 01:15:09 -0400

    Add files via upload

---
+++ Tutorial_1/tut1_skeleton.m
@@ -0,0 +1,92 @@
+% Tutorial 1 for EMBS - ANSWERS
+% By Binh Nguyen
+
+
+% A certain automobile manufacturer claims that its deluxe sports car will 
+% accelerate from rest to a speed of 200km/hr in 10.00 s. 
+
+%% Part 1 
+%%%%% a)
+
+% Define your variables
+v_kmh = 
+t_s = 
+
+% Display your variables
+disp ('Velocity is:');
+disp ();
+disp ('Time is:');
+disp ();
+
+% Convert v_kmh to v_ms
+v_ms = 
+disp ('Velocity in m/s is:')
+disp (v_ms);
+
+%%%%% b)
+a_avg = 
+disp ('Avg Accel is:')
+disp (a_avg);
+
+%%%%% c)
+delta_x = 
+disp ('Distance in m is:')
+disp (delta_x);
+
+%%%%% d)
+% Reassign t to new value
+t_s = 
+v_10 = 
+
+% Convert back to km/h
+v_10_kmh = 
+
+disp ('Velocity at t=10 is:')
+disp(v_10_kmh);
+
+
+%% Part 2
+A = [1 2 3; 4 5 6; 7 8 9]; % Setting a matrix A
+B = [1 2 3; 4 5 6; 7 8 9]; % Setting a matrix B
+
+% A * inverse B
+% .* is to multiply corresponding indices/ element by element matching
+%  * is matrix multiplication
+
+%%%%% a) MULTIPLICATION
+output = -2.*A;
+disp (output)
+
+%%%%% b) SQUARING
+% Method 1
+output = 
+disp (output)
+
+% Method 2
+output = 
+disp (output)
+
+%%%%% c) '.*' vs '*'
+output = 
+disp(output)
+
+output = 
+disp(output)
+
+
+%% Part 3
+far = 100
+disp (f_to_c (far))
+
+far = input ('What is your temperture in (c)')
+disp (f_to_c (far))
+
+
+
+
+
+
+
+
+
+

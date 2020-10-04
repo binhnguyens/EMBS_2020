f37afea484bb91070ef34089432bcd542e1d22af
Binh Nguyen <binhnguyens@live.ca>
2020-10-04 17:22:50 -0400

    Add files via upload

---
+++ Tutorial_2/tut2_skeleton.m
@@ -0,0 +1,71 @@
+% Tutorial 2 for MATLAB
+
+%%%%%% Part 1
+%% Set Matrix A and B 
+A = [1 2 3; 4 5 6; 7 8 9];
+B = [9 8 7; 6 5 4; 3 2 1];
+disp(A);
+disp(B);
+
+%% Find B transpose
+
+
+%% A * B'
+
+
+%% Find determinant of A
+
+
+%% Find A(3,2)
+
+
+%%%%%% Part 2
+%% Frankenstein a Matrix E where:
+% The first two columns are the first two columns of A  
+% The third column is the second row of B 
+
+% Fill in the below
+first_col = 
+sec_col =
+third_col = 
+E = 
+disp(E);
+
+%% Inverse of E
+
+
+%% Sum of matrix E 
+
+
+
+%%%%%% Part 3
+%% Create a single plot of sin^2 (x) and cos (x^2)
+% Solution:
+% Our goal in this section is to create two plots on one
+% Create your range from 0 to 2 pi with a step of pi/100
+% start:step:end
+
+% Answer here
+
+% Creat both the equations y1 and y2
+% Must use .^ for exponent since you're 
+
+% Answer here
+
+%%%%%% Explaintation of code
+% figure - function to open a window so that you can plot stuff
+%           If you don't use this function, then if you try to plot new
+%           figures, then it'll overlap on the same figure.
+% 
+% hold on - function that allows the figure to "hold on" so you could   
+%           plot multiple different signals on.
+% 
+% plot - plot something on the figure.
+% 
+% xlabel and ylabel - to label the axes.
+% 
+% xlim and ylim - set the limits of the axes. syntax is as follows ([min max])
+% 
+% legend and title - labelling of legend and title of the figure
+
+% Answer here
